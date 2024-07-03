5bfa86d415409606f9a2e1a09a488982d3aa1623
Niramay27 <niramaympatel@gmail.com>
2024-07-03 11:19:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,6 @@ app.get('/', (req, res) => {
   res.sendFile('index.html',{root:__dirname})
 })
 
-app.get("/api",async(req,res)=>{
-  console.log(req.__parsedUrl.query)
-  let url = "https://newsapi.org/v2/everything?"+req.__parsedUrl.query
-  let r = await axios(url)
-  let a = r.data
-  res.json(a)
-})
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
