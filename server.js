210c11889bc6db8539832c01a67f6e1563300811
Niramay27 <niramaympatel@gmail.com>
2024-07-03 10:26:31 +0530

    server site request

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+const { default: axios } = require('axios')
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile('index.html',{root:__dirname})
+})
+
+app.get("/api",async(req,res)=>{
+  console.log(req.__parsedUrl.query)
+  let url = "https://newsapi.org/v2/everything?"+req.__parsedUrl.query
+  let r = await axios(url)
+  let a = r.data
+  res.json(a)
 })
 
 app.listen(port, () => {
