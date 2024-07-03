7f1e40b457959834d92bf405fe0e45e7af33d22b
Niramay27 <142372471+Niramay27@users.noreply.github.com>
2024-07-03 22:32:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html',{root:__dirname})
+app.get('/api', async(req, res) => {
+  console.log(req.__parsedUrl.query)
+  let url = "https://newsapi.org/v2/everything?"+req.__parsedUrl.query
+  let r = await axios(url)
+  let a = r.data
+  res.json(a)
 })
 
 app.listen(port, () => {
